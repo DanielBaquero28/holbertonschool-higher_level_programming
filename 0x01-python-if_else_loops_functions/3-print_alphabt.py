fe68ae9013a467861fe2b019ca119b7fddd98a63
DanielBaquero28 <835@holbertonschool.com>
2019-09-10 22:25:41 +0000

    Fixed [With continue function]

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord("a"), ord("z") + 1):
-    if(i != 113 or i != 101)
-    print("{:s}".format(chr(i)), end="")
+    if(i == 101 or i == 113):
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
