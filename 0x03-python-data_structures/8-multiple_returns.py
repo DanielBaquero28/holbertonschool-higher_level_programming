a2553b1ddf9de7087f1b4f66cec07ed9180040e7
DanielBaquero28 <835@holbertonschool.com>
2019-09-17 23:59:59 +0000

    idk

--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    len_string = len(sentence)
-    if len_string == 0:
-        first_character = "None"
-    else:
-        first_character = sentence[0]
-
-    return (len_string, first_character)
