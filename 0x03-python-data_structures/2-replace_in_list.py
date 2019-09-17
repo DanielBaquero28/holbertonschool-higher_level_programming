6571109e14a3e34b74eda213049dc927f4ae929e
DanielBaquero28 <835@holbertonschool.com>
2019-09-17 14:42:11 -0500

    Feature [First Try]

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        print(my_list)
+    else:
+        my_list = my_list.replace(my_list[idx], element)
+        print(my_list)
