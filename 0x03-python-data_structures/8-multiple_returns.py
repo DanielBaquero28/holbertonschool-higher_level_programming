bc9107c00221eae92d748b2d41d85925b79b0410
DanielBaquero28 <835@holbertonschool.com>
2019-09-18 00:24:46 +0000

    Fixed [Some style issues]

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,5 +6,4 @@ def multiple_returns(sentence):
     else:
         first_character = sentence[0]
 
-
     return (len_string, first_character)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,6 @@ def max_integer(my_list=[]):
     if len_list == 0:
         return (None)
 
-
     my_list.sort()
     max_number = my_list[-1]
     return (max_number)
