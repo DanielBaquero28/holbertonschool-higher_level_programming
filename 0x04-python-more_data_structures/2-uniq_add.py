f28d96c1e2376ee940b4736dddc1c953732c41aa
DanielBaquero28 <835@holbertonschool.com>
2019-09-19 20:11:40 +0000

    Featuure [Storing the result in a new variable]

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,6 @@ from functools import reduce
 
 
 def uniq_add(my_list=[]):
-    my_list = set(my_list)
+    my_list2 = set(my_list)
 
-    return (reduce((lambda x, y: x+y), my_list))
+    return (reduce((lambda x, y: x+y), my_list2))
