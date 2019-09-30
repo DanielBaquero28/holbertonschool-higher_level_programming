8068e6e9cfeedb8d1d716858487c7bf0fce7ca71
DanielBaquero28 <835@holbertonschool.com>
2019-09-30 19:05:57 +0000

    Fixed [Removed except]

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,6 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except IndexError:
-            print("list index out of range")
-            raise
         except ValueError:
             pass
         except TypeError:
