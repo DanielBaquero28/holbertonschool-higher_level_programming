372128ba092a97e239f5e2cd74175fe1c2af118f
DanielBaquero28 <835@holbertonschool.com>
2019-10-01 23:29:34 +0000

    Feature [Added a method]

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """This is a class defining a square.
+    """
+    def __init__(self, size):
+        """Declares the method where the size of the sqaure is defined.
+        Args:
+            param1 (int): The size of the square
+        """
+        self.__size = size
