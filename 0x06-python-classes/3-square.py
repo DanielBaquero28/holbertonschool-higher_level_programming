52527c7d3c371688c798d03f510e91571054d831
DanielBaquero28 <835@holbertonschool.com>
2019-10-02 00:11:45 +0000

    Feature [Defined another mehtod that returns area of the square]

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+class Square:
+    """This is a class defining a square.
+    """
+    def __init__(self, size=0):
+        """Declares the method where the size of the sqaure is defined.
+        Args:
+            param1 (int): The size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Declares the method where the area of a square is returned.
+        """
+        return(self._size * self.__size)
