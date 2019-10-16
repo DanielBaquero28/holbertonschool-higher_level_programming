531182ab4cba9139a93d8c1837e6ae288220f702
DanielBaquero28 <835@holbertonschool.com>
2019-10-16 01:12:08 +0000

    Feature [super() is nice]

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Inherits from BaseGeometry. """
+    def __init__(self, size):
+        """ Constructor method """
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """ Returns the area of the Rectangle. """
+
+        return (super().area())
