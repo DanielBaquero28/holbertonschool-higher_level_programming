93713f59e35edadbd01168891e76a61f3bee5539
DanielBaquero28 <835@holbertonschool.com>
2019-10-17 00:05:13 +0000

    Featue [Organized code]

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import math
+
+
+def combination(n, r):
+    return (int((math.factorial(n)) / ((math.factorial(r)) * math.factorial(n - r))))
+
+def pascal_triangle(n):
+    triangle = []
+    for count in range(n):
+        row = []
+        for item in range(count + 1):
+            row.append(combination(count, item))
+        triangle.append(row)
+
+    return (triangle)
