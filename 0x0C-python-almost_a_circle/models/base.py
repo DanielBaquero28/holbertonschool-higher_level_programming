7cae4f889de2be6a50580f04a6116f6cc1b87628
DanielBaquero28 <835@holbertonschool.com>
2019-10-19 21:55:17 +0000

    Feature [Updated with to_json_string method that changes json object datastructure to string]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Importing module json in order to manipulate it """
+import json
+
+
 class Base:
     """ Base Class """
     __nb_objects = 0
@@ -9,3 +13,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns an empty list if list_dictionaries is equal to None, else
+        returns JSON string representation it.
+        """
+        if list_dictionaries is None:
+            return ("[]")
+        if not type(list_dictionaries) == list:
+            raise TypeError("list_dictionaries must be a list")
+        for i in list_dictionaries:
+            if not type(i) == dict:
+                raise TypeError("Must be a list of dictionaries")
+
+        string_json = json.dumps(list_dictionaries)
+        return (string_json)
