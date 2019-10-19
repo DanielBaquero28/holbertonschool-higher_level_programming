1d4c969936bbf27ccf5c8acab7db732e28cf24f0
DanielBaquero28 <835@holbertonschool.com>
2019-10-19 18:40:50 +0000

    Feature [Updated with display method that prints the rectangle]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,3 +71,8 @@ class Rectangle(Base):
     def area(self):
         """ Gets the value of the area of the rectangle """
         return (self.__width * self.__height)
+
+    def display(self):
+        """ Prints in stdout the rectangle with '#' """
+        for i in range(self.__height):
+            print("#" * self.__width)
