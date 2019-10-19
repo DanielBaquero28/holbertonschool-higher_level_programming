6cc41b7e5e516db906e6f61b7bf5168b523f92e6
DanielBaquero28 <835@holbertonschool.com>
2019-10-19 21:04:13 +0000

    Feature [Added update method because of size]

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,29 @@ class Square(Rectangle):
         """ Sets the value of size with setter decorator """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Assigns an key/value argument to each corresponding method """
+        num_arg = 0
+        if len(args) != 0 and args:
+            for arg in args:
+                if num_arg == 0:
+                    self.id = args[0]
+                elif num_arg == 1:
+                    self.size = args[1]
+                elif num_arg == 2:
+                    self.x = args[2]
+                elif num_arg == 3:
+                    self.y = args[3]
+                num_arg += 1
+            return
+
+        for key in kwargs:
+            if key == "id":
+                self.id = kwargs["id"]
+            if key == "size":
+                self.size = kwargs["size"]
+            if key == "x":
+                self.x = kwargs["x"]
+            if key == "y":
+                self.y = kwargs["y"]
