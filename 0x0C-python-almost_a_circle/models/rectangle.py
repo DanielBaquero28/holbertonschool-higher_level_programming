bb6358081f2ea545698bc191f684542b399d0876
DanielBaquero28 <835@holbertonschool.com>
2019-10-19 18:54:43 +0000

    Feature [Updated with __str__ method]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,8 @@ class Rectangle(Base):
         """ Prints in stdout the rectangle with '#' """
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """ Magic method that returns an object string representation """
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                        self.__y, self.__width, self.__height))
