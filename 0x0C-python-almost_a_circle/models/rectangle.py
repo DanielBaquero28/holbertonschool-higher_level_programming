f9dfb4d3b0497663da7c68b2a682c75cee97f6db
DanielBaquero28 <835@holbertonschool.com>
2019-10-19 18:16:45 +0000

    Feature [Updated rectangle method adding validation to setter methods]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,64 @@ class Rectangle(Base):
     """ Subclass that inherits from Base """
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Class constructor method of Rectangle """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """ Gets the value of width with property decorator """
+        return (self.__width)
+
+    @width.setter
+    def width(self, name, value):
+        """ Sets the value of width with setter decorator """
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Gets the value of height with property decorator """
+        return (self.__width)
+
+    @height.setter
+    def height(self, name, value):
+        """ Sets the value of height with setter decorator """
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Gets the value of x with property decorator """
+        return (self.__x)
+
+    @x.setter
+    def x(self, name, value):
+        """ Sets the value of x with setter decorator """
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Gets the value of y with property decorator """
+        return (self.__y)
+
+    @y.setter
+    def y(self, name, value):
+        """ Sets the value of y with setter decorator """
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        self.__y = value
