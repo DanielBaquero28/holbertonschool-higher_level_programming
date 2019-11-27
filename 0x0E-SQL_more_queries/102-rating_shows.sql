e4de4f65702f7695ffdeed6e4f24cd5439d62f0c
DanielBaquero28 <835@holbertonschool.com>
2019-11-27 03:40:58 +0000

    [Feature] Version #3

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,17 @@
+-- Lists all shows by their rating
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT
+	tv_shows.title,
+	SUM(tv_show_ratings.rate)
+AS
+	rating
+FROM
+	tv_shows
+INNER JOIN
+	tv_show_ratings
+ON
+	tv_show_ratings.show_id = tv_shows.id
+GROUP BY
+	tv_shows.title
+ORDER BY
+	rating DESC;
