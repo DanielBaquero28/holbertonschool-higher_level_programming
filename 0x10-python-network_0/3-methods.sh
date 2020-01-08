435ad66c78d9bf35f0f6a29c0b15c7e5df6ff6d8
Daniel Baquero <835@holbertonschool.com>
2020-01-08 00:03:30 +0000

    [Fixed] Missing :

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -s -i $1 | grep "Allow:" | sed "s/Allow //g"
+curl -s -i $1 | grep Allow: | sed "s/Allow: //g"
