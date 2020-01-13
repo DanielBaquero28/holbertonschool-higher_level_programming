80433c257c387d81798b49f20b2eeffd0f575477
Daniel Baquero <835@holbertonschool.com>
2020-01-13 22:21:16 +0000

    [Fixed] Missing error handling

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,5 +5,8 @@ import requests
 
 
 if __name__ == '__main__':
-    req = requests.get(sys.argv[1])
-    print(req.headers['X-Request-Id'])
+    try:
+        req = requests.get(sys.argv[1])
+        print(req.headers['X-Request-Id'])
+    except:
+        pass
