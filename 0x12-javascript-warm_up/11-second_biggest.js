9c2d78f60116aedcd59b79fb2f6143515c88d008
Daniel Baquero <835@holbertonschool.com>
2020-02-05 02:39:46 +0000

    [Feature]: Testing

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,10 @@
 function secondMax (a, b) {
   return (a - b);
 }
-
 if (process.argv.length < 4) {
   console.log(0);
 } else {
   let x = process.argv.slice(2);
   x.sort(secondMax);
-  console.log(x[x.length -2]);
+  console.log(x[x.length - 2]);
 }
