e68bcee956a590e6f593306d3ffc1612fddf19b7
Daniel Baquero <835@holbertonschool.com>
2020-02-05 03:54:46 +0000

    [Fixed]: Changed variable datatype

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ function secondMax (a, b) {
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  let x = process.argv.slice(2);
+  const x = process.argv.slice(2);
   x.sort(secondMax);
   console.log(x[x.length - 2]);
 }
