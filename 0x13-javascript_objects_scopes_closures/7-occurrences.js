d29e45f787d76855c6797fbdd345a159845c1bfe
Daniel Baquero <835@holbertonschool.com>
2020-02-10 23:52:17 +0000

    [Feature]: Testing

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  const listO = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      listO.append(list[i])
+    }
+  }
+  console.log(listO.length);
+}
