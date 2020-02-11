e5267a6b1e7027e022163af2a6b86b5c3baa60ef
Daniel Baquero <835@holbertonschool.com>
2020-02-11 17:24:00 +0000

    [Feature]: Sync programming way

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
 const file = require('fs');
 
-const content;
-file.readFile(process.argv[2], function (error, data) {
+file.readFileSync(process.argv[2], 'utf-8', function (error, data) {
   if (error) {
     throw error;
   }
-  content = data;
-
-  processFile();
+  console.log(data);
 });
-
-function processFile() {
-  console.log(content);
-}
