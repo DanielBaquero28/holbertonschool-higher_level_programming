7318c84d500612953ba2a01be4eb241d2250ad04
Daniel Baquero <835@holbertonschool.com>
2020-02-11 17:58:54 +0000

    [Feature]: Testing

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], function (response) {
-  console.log('code:', response.statusCode);
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
