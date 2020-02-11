6eae020973266f80588f3c70d62ccd274d622ed1
Daniel Baquero <835@holbertonschool.com>
2020-02-11 02:24:39 +0000

    [Feature]: Updated

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Map = require('./100-data').list;
+const list = require('./100-data').list;
 
 for (let i = 0; i < list.length; i++) {
   const newArray = list.map(x => x * i);
