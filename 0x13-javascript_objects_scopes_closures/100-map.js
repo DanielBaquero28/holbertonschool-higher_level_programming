ba9b264408b981fa3b56f59e0f407840e54dd87a
Daniel Baquero <835@holbertonschool.com>
2020-02-11 02:27:07 +0000

    [Fixed]: Changed logic

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-for (let i = 0; i < list.length; i++) {
-  const newArray = list.map(x => x * i);
-}
+const newArray = list.map(function (n, index) {
+  return (n * index);
+});
 
 console.log(list);
 console.log(newArray);
