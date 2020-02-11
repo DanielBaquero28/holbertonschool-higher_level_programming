679b005e3bdcd7ae2afc7417950280ae3df8c1c4
Daniel Baquero <835@holbertonschool.com>
2020-02-11 17:31:02 +0000

    [Fixing]: Checkers

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFileSync(process.argv[2], 'utf-8', (error, data) => {
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
   if (error) {
     console.log(error);
   }
