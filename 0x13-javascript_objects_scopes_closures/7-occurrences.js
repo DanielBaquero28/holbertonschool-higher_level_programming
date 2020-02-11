6800d3649b7999b26b085c99b863f51a57523934
Daniel Baquero <835@holbertonschool.com>
2020-02-11 00:00:08 +0000

    [Fixing]: Test

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   const listO = [];
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-      listO.append(list[i])
+      listO.push(list[i])
     }
   }
   return (listO.length);
