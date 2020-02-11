a2b0b9ea1a2be5212cc7c9a78e463e74ffe709d0
Daniel Baquero <835@holbertonschool.com>
2020-02-11 00:30:35 +0000

    [Fixed]: Logic

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const listRev = [];
-  for (let i = list.length - 1; i < list.length; i--) {
-    listRev.push(list[i]);
+  for (let i = 0, j = list.length - 1; i < list.length; i++, j--) {
+    listRev[i] = list[j];
   }
   return (listRev);
 };
