de07835497e4fc81f6b55738d8277b05725ae703
Daniel Baquero <835@holbertonschool.com>
2020-02-11 17:35:18 +0000

    [Fixed]: Semi-Standard JS Style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (error, data) => {
   if (error) {
     console.log(error);
-  }
-  else {
+  } else {
     console.log(data.toString());
   }
 });
