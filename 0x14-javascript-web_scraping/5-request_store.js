130d9af676bed4dc1353b56ef3aac3a8d3cfdf1f
Daniel Baquero <835@holbertonschool.com>
2020-02-11 20:38:00 +0000

    [Feature]: Semi-standard STYLE

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-const requestURL = process.argv[2];
 
+const requestURL = process.argv[2];
 request(requestURL, function (error, response, body) {
   if (error) {
     console.error(error);
@@ -10,7 +10,7 @@ request(requestURL, function (error, response, body) {
     const content = body;
     fs.writeFile(process.argv[3], content, 'utf-8', (errorSec) => {
       if (errorSec) {
-	console.log(errorSec);
+        console.log(errorSec);
       }
     });
   }
